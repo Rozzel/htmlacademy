a16f04eebfbf157682572556a9adbb7d99953d82
Vladislav Golik <Vladislav.Golik@gmail.com>
2017-12-26 17:10:19 +0300

    localStorage

--- Формы и HTML5/LocalStorage/script.js
+++ Формы и HTML5/LocalStorage/script.js
@@ -1,15 +1,19 @@
-if (window.localStorage) {
-    var elements = document.querySelectorAll('[name]');
+function init() {
+    if (window.localStorage) {
+        var elements = document.querySelectorAll('[name]');
 
-    for (var i = 0, length = elements.length; i < length; i++) {
-        (function(element) {
-            var name = element.getAttribute('name');
+        for (var i = 0, length = elements.length; i < length; i++) {
+            (function(element) {
+                var name = element.getAttribute('name');
 
-            element.value = localStorage.getItem(name) || '';
+                element.value = localStorage.getItem(name) || '';
 
-            element.onkeyup = function() {
-                localStorage.setItem(name, element.value);
-            };
-        })(elements[i]);
+                element.onkeyup = function() {
+                    localStorage.setItem(name, element.value);
+                };
+            })(elements[i]);
+        }
     }
 }
+
+window.onload = init;
