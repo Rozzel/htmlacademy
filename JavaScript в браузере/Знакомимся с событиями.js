807431ac76d4cdacd2d43b09fcf5c4d24c1d2e7e
Vladislav Golik <Vladislav.Golik@gmail.com>
2018-12-27 12:52:27 +0300

    popup

--- JavaScript в браузере/Знакомимся с событиями.js
+++ JavaScript в браузере/Знакомимся с событиями.js
@@ -26,3 +26,26 @@ var popup = document.querySelector('.modal');
 https://developer.mozilla.org/en-US/docs/Web/Events
 
 */
+
+link.addEventListener('click', function(evt) {
+  // Отменяем действие по умолчанию
+  evt.preventDefault();
+
+  // Добавляем инструкции для события клика
+  console.log('Произошёл клик');
+});
+
+var popup = document.querySelector('.modal');
+var openPopupButton = document.querySelector('.button-open');
+
+openPopupButton.addEventListener('click', function (evt) {
+  
+  evt.preventDefault();
+  console.log('Клик по кнопке');
+});
+
+var popup = document.querySelector('.popup');
+// Перед названием селектора ставим точку
+
+popup.classList.remove('popup--open');
+// Перед названием класса точка не ставится
